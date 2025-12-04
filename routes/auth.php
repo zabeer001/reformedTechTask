74313a04b84286e7490093ad324a98a797e326ea
zabeer001 <binzabirtareq@gmail.com>
2025-12-04 14:16:00 +0600

    swagger a dn test is also done

--- routes/auth.php
+++ routes/auth.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/signin', [AuthController::class, 'signin'])->middleware('throttle:3,3');
     // Route::post('/register', [AuthController::class, 'register']);
+    //swagger a dn test is also done
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::post('/signout', [AuthController::class, 'signout']);
 });
